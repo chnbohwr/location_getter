83f663814316852d1002042db3023d25fd66e89f
chnbohwr <chnbohwr@gmail.com>
2016-10-16 19:39:39 +0800

    make query function

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+const fs = require('fs');
+const request = require('superagent');
+const jsonfile = require('jsonfile');
 const key = require('./key.json').key;
 const config = require('./config.json');
+
+function queryList(){
+    const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
+    function queryListEnd(err, res){
+        if (err) {
+            console.log('error', err);
+        } else {
+            var result = res.body;
+            console.log(res);
+            writeJSON('list.json', result);
+        }
+    }
+    request.get(url).query(config).query({key}).end(queryListEnd);
+}
+
+function writeJSON(fileName, object){
+    fs.mkdir('json', function(){
+        var filePath = './json/' + fileName;
+        jsonfile.writeFileSync(filePath, object);
+    })
+}
+
+queryList();
